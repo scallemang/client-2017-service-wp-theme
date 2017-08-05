02ea18f658084c28f2f4bed8546652dbe322421f
M. Leslie Bent <thevariables@gmail.com>
2017-08-05 17:59:06 -0400

    uploader handling

--- wp-content/themes/teledent-app/src/js/ControllerRegister.js
+++ wp-content/themes/teledent-app/src/js/ControllerRegister.js
@@ -2,217 +2,228 @@ angular.module('teledent', ['ngFileUpload'])
 .controller('RegistrationController', 
     ['$scope', '$http','$window', 'Upload', function($scope, $http, $window, Upload) {
 
-  // $scope.ajaxurl = $window.ajaxurl;
 
-  //Initial data states
-  $scope.master = {};
-  $scope.formState = 'applicant'; //register
-  $scope.user = {};
-  $scope.provList = [];
-  $scope.genderList = [];
-  $scope.languageList = [];
-  $scope.lcoationList = [];
-  $scope.contractTypesList = [];
-  $scope.workTypesObjekt = [];
-  $scope.softwareObjekt = [];
+  $scope.init = function() {
+    $scope.reset();
+    $scope.wpUploadFileHandler();
+    $scope.dataSetup();
+  }
 
-  $scope.emailIsUnique = true;
+  $scope.reset = function() {
+    $scope.user = angular.copy($scope.master);
+  };
 
-  $scope.user.province = "Ontario";
-  $scope.user.gender = "Prefer not to say";
-  
-  //WP Uploader placement hack.
-  //if wfu_container.length > 2, remove the first ones.
 
+  $scope.dataSetup = function() {
+
+    //Initial data states
+    $scope.master = {};
+    $scope.formState = 'applicant'; //register
+    $scope.user = {};
+    $scope.provList = [];
+    $scope.genderList = [];
+    $scope.languageList = [];
+    $scope.lcoationList = [];
+    $scope.contractTypesList = [];
+    $scope.workTypesObjekt = [];
+    $scope.softwareObjekt = [];
+
+    $scope.emailIsUnique = true;
+
+    $scope.user.province = "Ontario";
+    $scope.user.gender = "Prefer not to say";
+
+    //View Data
+      var provObjekt = [
+        {name:'Alberta'},
+        {name:'British Columbia'},
+        {name:'Manitoba'},
+        {name:'New Brunswick'},
+        {name:'British Columbia'},
+        {name:'Newfoundland and Labrador'},
+        {name:'Northwest Territories'},
+        {name:'Nova Scotia'},
+        {name:'Nunavut'},
+        {name:'Ontario'},
+        {name:'Prince Edward Island'},
+        {name:'Quebec'},
+        {name:'Saskatchewan'},
+        {name:'Yukon'}
+      ];
+
+      var genderObjekt = [
+        {name:'Female'},
+        {name:'Male'},
+        {name:'Other'},
+        {name:'Prefer not to say'}
+      ];
+
+      var languageObjekt = [
+        {name:'English'},
+        {name:'French'},
+        {name:'Arabic'},
+        {name:'Bengali'},
+        {name:'Cantonese'},
+        {name:'Croatian'},
+        {name:'Czech'},
+        {name:'Farsi'},
+        {name:'Filipino'},
+        {name:'German'},
+        {name:'Greek'},
+        {name:'Gujarati'},
+        {name:'Hebrew'},
+        {name:'Hindi'},
+        {name:'Italian'},
+        {name:'Japanese'},
+        {name:'Korean'},
+        {name:'Kurdish'},
+        {name:'Mandarin'},
+        {name:'Persian'},
+        {name:'Polish'},
+        {name:'Punjabi'},
+        {name:'Portuguese'},
+        {name:'Russian'},
+        {name:'Serbian'},
+        {name:'Spanish'},
+        {name:'Tagalog'},
+        {name:'Tamil'},
+        {name:'Turkish'},
+        {name:'Ukranian'},
+        {name:'Urdu'}
+      ];
+
+      var locationObjekt = [
+        {name:'Acton'},
+        {name:'Ajax'},
+        {name:'Ancaster'},
+        {name:'Aurora'},
+        {name:'Barrie'},
+        {name:'Bolton'},
+        {name:'Bowmanville'},
+        {name:'Bradford'},
+        {name:'Brampton'},
+        {name:'Brantford'},
+        {name:'Burlington'},
+        {name:'Caledon'},
+        {name:'Cambridge'},
+        {name:'Don Mills'},
+        {name:'East Gwillimbury'},
+        {name:'East York'},
+        {name:'Etobicoke'},
+        {name:'Georgetown'},
+        {name:'Grimsby'},
+        {name:'Guelph'},
+        {name:'Halton Hills'},
+        {name:'Hamilton'},
+        {name:'Innisfil'},
+        {name:'King'},
+        {name:'Kitchener'},
+        {name:'London'},
+        {name:'Malton'},
+        {name:'Maple'},
+        {name:'Markham'},
+        {name:'Milton'},
+        {name:'Mississauga'},
+        {name:'New Tecumseth'},
+        {name:'Newmarket'},
+        {name:'Niagara Falls'},
+        {name:'North York'},
+        {name:'Oakville'},
+        {name:'Orangeville'},
+        {name:'Oshawa'},
+        {name:'Pickering'},
+        {name:'Port Credit'},
+        {name:'Richmond Hill'},
+        {name:'Saint Catharines'},
+        {name:'Scarborough'},
+        {name:'Shelburne'},
+        {name:'Stouffville'},
+        {name:'Thornhill'},
+        {name:'Toronto'},
+        {name:'Vaughan'},
+        {name:'Waterdown'},
+        {name:'Waterloo'},
+        {name:'Welland'},
+        {name:'Weston'},
+        {name:'Whitby'},
+        {name:'Woodbridge'},
+        {name:'York'},
+        {name:'Other'}
+      ];
+
+      var contractTypesObjekt = [
+        {name:'Full-time'},
+        {name:' Part-time'},
+        {name:' Permanent'},
+        {name:'Temporary'}
+      ];
+
+      var workTypesObjekt = [
+        {name:'Assistant (Level I)'},
+        {name:'Assistant (Level II)'},
+        {name:'Orthodontic Assistant (Level I)'},
+        {name:'Orthodontic Assistant (Level II)'},
+        {name:'Floater (Level I)'},
+        {name:'Floater (Level II)'},
+        {name:'Hygiene Coordinator'},
+        {name:'Office Manager'},
+        {name:'Receptionist'},
+        {name:'Registered Dental Hygienist'},
+        {name:'Orthodontic Hygienist'},
+        {name:'Restorative Dental Hygienist'},
+        {name:'Treatment Coordinator'}
+      ];
+
+      var softwareObjekt = [
+        {name:'ABELDent'},
+        {name:'Adstra'},
+        {name:'Autopia'},
+        {name:'Carestream'},
+        {name:'ClearDent'},
+        {name:'DentalWare'},
+        {name:'DentiMax'},
+        {name:'Dentrix'},
+        {name:'DOM'},
+        {name:'EagleSoft'},
+        {name:'Exan'},
+        {name:'iTero'},
+        {name:'LiveDDM'},
+        {name:'Logic Tech/Paradigm'},
+        {name:'MacPractice'},
+        {name:'Maxident'},
+        {name:'OPES'},
+        {name:'OrthoTrac'},
+        {name:'TDO'},
+        {name:'Tracker'},
+        {name:'Other'}
+      ];
+
+      $scope.provList = provObjekt;
+      $scope.genderList = genderObjekt;
+      $scope.languageList = languageObjekt;
+      $scope.locationList = locationObjekt;
+      $scope.contractTypesList = contractTypesObjekt;
+      $scope.workTypesList = workTypesObjekt;
+      $scope.softwareList = softwareObjekt;
+  }
+
+  //
   $scope.wpUploadFileHandler = function () {
-    
     var uploaders = document.querySelectorAll('.wfu_container');
     var destination = document.getElementById('putUpload');
-    if(uploaders.length > 1) {
-      uploaders = document.querySelectorAll('.wfu_container')[0];
+
+    try {
+      $scope.uploaderHTML = uploaders[0].innerHTML;
+      for(var i = 0; i < Object.keys(uploaders).length; i++) {
+        document.querySelectorAll('.wfu_container')[0].remove();
+      }
     }
 
-    var uploader = p.cloneNode(uploaders);
-    uploaders.remove();
-    destination.innerHTML = uploader;
-
+    try {      
+        destination.innerHTML = $scope.uploaderHTML;
+    }
 
   }
- 
-
-  //View Data
-  var provObjekt = [
-    {name:'Alberta'},
-    {name:'British Columbia'},
-    {name:'Manitoba'},
-    {name:'New Brunswick'},
-    {name:'British Columbia'},
-    {name:'Newfoundland and Labrador'},
-    {name:'Northwest Territories'},
-    {name:'Nova Scotia'},
-    {name:'Nunavut'},
-    {name:'Ontario'},
-    {name:'Prince Edward Island'},
-    {name:'Quebec'},
-    {name:'Saskatchewan'},
-    {name:'Yukon'}
-  ];
-
-  var genderObjekt = [
-    {name:'Female'},
-    {name:'Male'},
-    {name:'Other'},
-    {name:'Prefer not to say'}
-  ];
-
-  var languageObjekt = [
-    {name:'English'},
-    {name:'French'},
-    {name:'Arabic'},
-    {name:'Bengali'},
-    {name:'Cantonese'},
-    {name:'Croatian'},
-    {name:'Czech'},
-    {name:'Farsi'},
-    {name:'Filipino'},
-    {name:'German'},
-    {name:'Greek'},
-    {name:'Gujarati'},
-    {name:'Hebrew'},
-    {name:'Hindi'},
-    {name:'Italian'},
-    {name:'Japanese'},
-    {name:'Korean'},
-    {name:'Kurdish'},
-    {name:'Mandarin'},
-    {name:'Persian'},
-    {name:'Polish'},
-    {name:'Punjabi'},
-    {name:'Portuguese'},
-    {name:'Russian'},
-    {name:'Serbian'},
-    {name:'Spanish'},
-    {name:'Tagalog'},
-    {name:'Tamil'},
-    {name:'Turkish'},
-    {name:'Ukranian'},
-    {name:'Urdu'}
-  ];
-
-  var locationObjekt = [
-    {name:'Acton'},
-    {name:'Ajax'},
-    {name:'Ancaster'},
-    {name:'Aurora'},
-    {name:'Barrie'},
-    {name:'Bolton'},
-    {name:'Bowmanville'},
-    {name:'Bradford'},
-    {name:'Brampton'},
-    {name:'Brantford'},
-    {name:'Burlington'},
-    {name:'Caledon'},
-    {name:'Cambridge'},
-    {name:'Don Mills'},
-    {name:'East Gwillimbury'},
-    {name:'East York'},
-    {name:'Etobicoke'},
-    {name:'Georgetown'},
-    {name:'Grimsby'},
-    {name:'Guelph'},
-    {name:'Halton Hills'},
-    {name:'Hamilton'},
-    {name:'Innisfil'},
-    {name:'King'},
-    {name:'Kitchener'},
-    {name:'London'},
-    {name:'Malton'},
-    {name:'Maple'},
-    {name:'Markham'},
-    {name:'Milton'},
-    {name:'Mississauga'},
-    {name:'New Tecumseth'},
-    {name:'Newmarket'},
-    {name:'Niagara Falls'},
-    {name:'North York'},
-    {name:'Oakville'},
-    {name:'Orangeville'},
-    {name:'Oshawa'},
-    {name:'Pickering'},
-    {name:'Port Credit'},
-    {name:'Richmond Hill'},
-    {name:'Saint Catharines'},
-    {name:'Scarborough'},
-    {name:'Shelburne'},
-    {name:'Stouffville'},
-    {name:'Thornhill'},
-    {name:'Toronto'},
-    {name:'Vaughan'},
-    {name:'Waterdown'},
-    {name:'Waterloo'},
-    {name:'Welland'},
-    {name:'Weston'},
-    {name:'Whitby'},
-    {name:'Woodbridge'},
-    {name:'York'},
-    {name:'Other'}
-  ];
-
-  var contractTypesObjekt = [
-    {name:'Full-time'},
-    {name:' Part-time'},
-    {name:' Permanent'},
-    {name:'Temporary'}
-  ];
-
-  var workTypesObjekt = [
-    {name:'Assistant (Level I)'},
-    {name:'Assistant (Level II)'},
-    {name:'Orthodontic Assistant (Level I)'},
-    {name:'Orthodontic Assistant (Level II)'},
-    {name:'Floater (Level I)'},
-    {name:'Floater (Level II)'},
-    {name:'Hygiene Coordinator'},
-    {name:'Office Manager'},
-    {name:'Receptionist'},
-    {name:'Registered Dental Hygienist'},
-    {name:'Orthodontic Hygienist'},
-    {name:'Restorative Dental Hygienist'},
-    {name:'Treatment Coordinator'}
-  ];
-
-  var softwareObjekt = [
-    {name:'ABELDent'},
-    {name:'Adstra'},
-    {name:'Autopia'},
-    {name:'Carestream'},
-    {name:'ClearDent'},
-    {name:'DentalWare'},
-    {name:'DentiMax'},
-    {name:'Dentrix'},
-    {name:'DOM'},
-    {name:'EagleSoft'},
-    {name:'Exan'},
-    {name:'iTero'},
-    {name:'LiveDDM'},
-    {name:'Logic Tech/Paradigm'},
-    {name:'MacPractice'},
-    {name:'Maxident'},
-    {name:'OPES'},
-    {name:'OrthoTrac'},
-    {name:'TDO'},
-    {name:'Tracker'},
-    {name:'Other'}
-  ];
-
-  $scope.provList = provObjekt;
-  $scope.genderList = genderObjekt;
-  $scope.languageList = languageObjekt;
-  $scope.locationList = locationObjekt;
-  $scope.contractTypesList = contractTypesObjekt;
-  $scope.workTypesList = workTypesObjekt;
-  $scope.softwareList = softwareObjekt;
-
   
   //1. Check for Unique Email - AJAXed
   $scope.emailCheck = function(user) {
@@ -383,11 +394,12 @@ $scope.uploadFileNO = function (file, user) {
 
   };
 
-  $scope.reset = function() {
-    $scope.user = angular.copy($scope.master);
-  };
 
-  $scope.reset();
+  
+
+
+  $scope.init();
+
 }])
 .directive("fileread", [function () {
     return {
